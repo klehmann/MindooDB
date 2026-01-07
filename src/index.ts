5db651ce61d079c23abac4661b0b584fe3dfae9d
Karsten Lehmann <karsten.lehmann@mindoo.de>
2026-01-07 21:47:32 +0100

    Renamed class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { BaseMindooDB } from "./BaseMindooDB";
 export { BaseMindooTenant } from "./BaseMindooTenant";
-export { BaseTenantFactory } from "./BaseTenantFactory";
+export { BaseMindooTenantFactory } from "./BaseMindooTenantFactory";
 export { InMemoryAppendOnlyStore, InMemoryAppendOnlyStoreFactory } from "./appendonlystores/InMemoryAppendOnlyStore";
 export { CryptoAdapter, BrowserCryptoAdapter, NodeCryptoAdapter, createCryptoAdapter } from "./crypto/CryptoAdapter";
 export { KeyBag } from "./keys/KeyBag";
