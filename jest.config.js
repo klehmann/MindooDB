8d53ab7a700e1079366e10136223b8235c025605
Karsten Lehmann <karsten.lehmann@mindoo.de>
2026-02-17 23:33:27 +0100

    Fixed test execution for MindooDBServer

--- jest.config.js
+++ jest.config.js
@@ -5,11 +5,13 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
   testPathIgnorePatterns: ['<rootDir>/src/__tests__/browser/'],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
   },
   // Handle .js extensions in imports (for ESM/Node16 resolution)
+  // and map mindoodb/* package imports to local source for example server tests
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^mindoodb/(.*)$': '<rootDir>/src/$1',
   },
   collectCoverageFrom: [
     'src/**/*.ts',
