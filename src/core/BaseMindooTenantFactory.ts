8cba0d34da957ac6026115ff9adf9c0e2dfffb95
Karsten Lehmann <karsten.lehmann@mindoo.de>
2026-08-14 22:53:24 +0200

    Build fix

--- src/core/BaseMindooTenantFactory.ts
+++ src/core/BaseMindooTenantFactory.ts
@@ -774,14 +774,6 @@ export class BaseMindooTenantFactory implements MindooTenantFactory {
     };
   }
 
-  private uint8ArrayToBase64(bytes: Uint8Array): string {
-    let binary = "";
-    for (let i = 0; i < bytes.length; i++) {
-      binary += String.fromCharCode(bytes[i]!);
-    }
-    return btoa(binary);
-  }
-
   private async importRsaWrappedJoinKeys(
     response: JoinResponse,
     options: JoinTenantOptions,
